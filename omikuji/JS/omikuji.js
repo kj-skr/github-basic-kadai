20380d27c1c6fd8f1d1813c11d738cf9beb2e4c3
kj-skr <kenjinopc@yahoo.co.jp>
2024-10-05 22:11:06 +0900

    Add files via upload

---
+++ omikuji/JS/omikuji.js
@@ -0,0 +1,24 @@
+let num = Math.floor(Math.random() * 5);
+
+console.log(num);
+
+if(num === 0){
+    alert('大吉');
+}
+
+else if(num === 1){
+    alert('吉');
+}
+
+else if(num === 2){
+    alert('中吉');
+}
+
+else if(num === 3){
+    alert('小吉');
+}
+
+else if(num === 4){
+    alert('末吉');
+}
+
